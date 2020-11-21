24190295577ca800caf396a14de3aff42ead719e
lergv <gitpy2580@gmail.com>
2020-11-21 09:30:34 +0100

    ok

--- web.py
+++ web.py
@@ -8,17 +8,30 @@ import random #defaultne nainstalovany v python 3
 #pip install matplotlib
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
-from flask import send_file
+from flask import send_file, session
+from flask_session import Session
 
 config = configparser.ConfigParser()
 config.read('./settings/config_local.ini')
 print(config['DATABASE']['STRING'])
 
+
+
 app = Flask(__name__)
+SESSION_TYPE = 'filesystem'
+app.config.from_object(__name__)
+Session(app)
+app.config["CACHE_TYPE"] = "null"
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 @app.route('/')#,methods=['POST','GET']
 def index():
-    return render_template("index.html")
+    session['sequence'] = 0
+
+
+    session['xs'] = range(100)
+    session['ys'] = [random.randint(1, 50) for x in session['xs']]
+    return render_template("index.html",len = len(session['ys']),Pokemons =session['ys'])
 
 @app.route('/fig/')
 def fig():
@@ -32,12 +45,14 @@ def fig():
 
 
 def create_figure():
+    #global xs
     fig = Figure()
     axis = fig.add_subplot(1, 1, 1)
-    xs = range(100)
-    ys = [random.randint(1, 50) for x in xs]
-    axis.plot(xs, ys)
+    axis.plot(session['xs'], session['ys'])
     return fig
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
+    
+    
